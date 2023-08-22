77ca8bedb588b8ac330b7931b6a58867645705ad
KJ Osenenko <karljosenenko@gmail.com>
2023-08-21 19:28:46 -0600

    Add image to show

--- shows/models.py
+++ shows/models.py
@@ -1,4 +1,7 @@
+from django.core.files.storage import FileSystemStorage
 from django.db import models
+
+fs = FileSystemStorage(location="/media")
 
 class Show(models.Model):
   venue = models.CharField(max_length=128, blank=True, null=True)
@@ -8,3 +11,4 @@ class Show(models.Model):
   show_date_time = models.DateTimeField(auto_now=False, auto_now_add=False, null=True)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+  image = models.ImageField(upload_to=fs, null=True)
