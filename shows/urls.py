fd0eae791ea6678781fb1ab89e1ea80dceebf9f0
KJ Osenenko <karljosenenko@gmail.com>
2024-01-21 17:23:59 -0700

    separate show endpoints

--- shows/urls.py
+++ shows/urls.py
@@ -3,4 +3,5 @@ from shows import views
 
 urlpatterns = [
   path('shows/', views.shows),
+  path('past_shows/', views.past_shows),
 ]
--- shows/views.py
+++ shows/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.utils.timezone import localdate
 from .models import Show
 from .serializers import ShowSerializer
 
 def shows(request):
   if request.method == 'GET':
-    shows = Show.objects.all()
+    today = localdate()
+    shows = Show.objects.filter(show_date_time__gte=today)
+    serializer = ShowSerializer(shows, many=True)
+    return JsonResponse(serializer.data, safe=False)
+  else:
+    return JsonResponse(serializer.errors, status=400)
+
+def past_shows(request):
+  if request.method == 'GET':
+    today = localdate()
+    shows = Show.objects.filter(show_date_time__lte=today)
     serializer = ShowSerializer(shows, many=True)
     return JsonResponse(serializer.data, safe=False)
   else:
