562e3d8c82a93a8a88eca57ba4cb694bde11b093
KJ Osenenko <karljosenenko@gmail.com>
2024-01-31 20:57:14 -0700

    redeclare static path

--- stilted/settings.py
+++ stilted/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-1ann9^h0s(8$e2763$$t&blc8j#06j^=awm*7-jdx&%_)is&v#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['api.stiltedband.com']
+ALLOWED_HOSTS = ['api.stiltedband.com','localhost']
 
 
 # Application definition
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
