7988fc921a6e41abe27049e5952454bf41efc514
KJ Osenenko <karljosenenko@gmail.com>
2024-02-20 21:44:18 -0700

    WIP cleanup local storage

--- shows/models.py
+++ shows/models.py
@@ -1,5 +1,7 @@
 from django.core.files.storage import FileSystemStorage
 from django.db import models
+from django.core.cache import cache
+from django.utils.timezone import localdate
 
 fs = FileSystemStorage(location="/tmp/pyhon_media")
 
@@ -11,3 +13,21 @@ class Show(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   image = models.ImageField(upload_to="shows", null=True)
+  
+  def current_shows():
+    today = localdate()
+    return Show.objects.filter(show_date_time__gte=today).extra(order_by=['show_date_time']).reverse()
+    
+  def past_shows():
+    today = localdate()
+    return Show.objects.filter(show_date_time__lt=today).extra(order_by=['show_date_time']).reverse()
+  
+  def cleanup():
+    t = 2419200
+    if cache.get('recently_run') == 'true':
+      return
+    else:
+      cache.set('recently_run', 'true', 10)
+      have_images = Show.past_shows().exclude(image='')
+      breakpoint()
+    
--- shows/views.py
+++ shows/views.py
@@ -1,23 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.utils.timezone import localdate
 from .models import Show
 from .serializers import ShowSerializer
 
 def shows(request):
   if request.method == 'GET':
-    today = localdate()
-    shows = Show.objects.filter(show_date_time__gte=today).extra(order_by=['show_date_time']).reverse()
-    serializer = ShowSerializer(shows, many=True)
+    serializer = ShowSerializer(Show.current_shows(), many=True)
     return JsonResponse(serializer.data, safe=False)
   else:
     return JsonResponse(serializer.errors, status=400)
 
 def past_shows(request):
   if request.method == 'GET':
-    today = localdate()
-    shows = Show.objects.filter(show_date_time__lt=today).extra(order_by=['show_date_time']).reverse()
-    serializer = ShowSerializer(shows, many=True)
+    serializer = ShowSerializer(Show.past_shows(), many=True)
     return JsonResponse(serializer.data, safe=False)
   else:
     return JsonResponse(serializer.errors, status=400)
