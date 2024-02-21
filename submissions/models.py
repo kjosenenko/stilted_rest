858995414a8b56fb424b5ccdf3cf313d9580bc02
KJ Osenenko <karljosenenko@gmail.com>
2024-02-20 20:16:36 -0700

    WIP submission post_save signal

--- submissions/models.py
+++ submissions/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Submission(models.Model):
   contact_name = models.CharField(max_length=128, blank=True, null=True)
@@ -8,3 +10,11 @@ class Submission(models.Model):
   message_body = models.TextField(blank=True, null=True)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+  
+@receiver(post_save, sender=Submission)
+def send_email(sender, instance, **kwargs):
+  print(instance.contact_name)
+  print(instance.contact_email)
+  print(instance.contact_phone)
+  print(instance.message_subject)
+  print(instance.message_body)
