1baf4bd7357d1a941c67bd9edc07dce15960760f
KJ Osenenko <kj@mac-kj.local>
2024-06-15 21:18:49 -0600

    WIP send email

--- submissions/models.py
+++ submissions/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import smtplib, ssl
 
 class Submission(models.Model):
   contact_name = models.CharField(max_length=128, blank=True, null=True)
@@ -18,3 +19,51 @@ def send_email(sender, instance, **kwargs):
   print(instance.contact_phone)
   print(instance.message_subject)
   print(instance.message_body)
+  
+  port = 465  # For SSL
+  smtp_server = "smtp.gmail.com"
+  sender_email = "stilted.band@gmail.com"  # Enter your address
+  receiver_email = "stilted.band@gmail.com"  # Enter receiver address
+  password = "" # Enter password
+  message = MIMEMultipart("alternative")
+  message["Subject"] = instance.message_subject
+  message["From"] = sender_email
+  message["To"] = receiver_email
+  
+  text = """\
+    New Reservation generated for:
+    Name: """ + instance.contact_name + """"
+    Reply to: """ + instance.contact_email + """"
+    Phone: """ + instance.contact_phone + """"
+
+    """ + instance.message_body
+    
+  html = """\
+  <html>
+    <body>
+      <p>
+        <b>Name:</b> """ + instance.contact_name + """<br>
+        <b>Reply to:</b> """ + instance.contact_email + """<br>
+        <b>Phone:</b> """ + instance.contact_phone + """<br><br>
+        """ + instance.message_body + """<br>
+      </p>
+    </body>
+  </html>
+  """       
+    
+  # Turn these into plain/html MIMEText objects
+  part1 = MIMEText(text, "plain")
+  part2 = MIMEText(html, "html")
+  
+  # Add HTML/plain-text parts to MIMEMultipart message
+  # The email client will try to render the last part first
+  message.attach(part1)
+  message.attach(part2)
+  
+  # Create secure connection with server and send email
+  context = ssl.create_default_context()
+  with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+      server.login(sender_email, password)
+      server.sendmail(
+          sender_email, receiver_email, message.as_string()
+      )
