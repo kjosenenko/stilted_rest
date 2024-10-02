4a5bcddc5f0ec2bce5358507ba54467f50f8f784
KJ Osenenko <kj@mac-kj.local>
2024-10-01 18:20:06 -0600

    require uniqueness for band attributes

--- bands/models.py
+++ bands/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Band(models.Model):
-  band_name = models.CharField(max_length=128, blank=True, null=True)
-  band_url = models.CharField(max_length=128, blank=True, null=True)
-  contact_email = models.CharField(max_length=128, blank=True, null=True)
+  band_name = models.CharField(max_length=128, blank=True, null=True, unique=True)
+  band_url = models.CharField(max_length=128, blank=True, null=True, unique=True)
+  contact_email = models.CharField(max_length=128, blank=True, null=True, unique=True)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
