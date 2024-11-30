68322c32f977eeda3d32471800a7f344ac3a783e
KJ Osenenko <kj@mac-kj.local>
2024-11-30 12:38:11 -0700

    WIP refactor submissions

--- submissions/urls.py
+++ submissions/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from submissions import views
 
 urlpatterns = [
-  path('contact', views.submit),
+  path('submit', views.submit),
 ]
--- submissions/views.py
+++ submissions/views.py
@@ -2,22 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
-from .serializers import SubmissionSerializer
+from bands.models import Band
 
 @csrf_exempt
 def submit(request):
   if request.method == 'POST':
     data = JSONParser().parse(request)
+    host = request.headers['X-Forwarded-Host']
+    band_id = Band.objects.get(url=host).id
+    # data['data']['band_id'] = band_id
+    breakpoint()
     
     # Comment in for React client.
     # serializer = SubmissionSerializer(data=data['data'])
-    
+
     # Comment in for Vue client.
-    serializer = SubmissionSerializer(data=data)
+    # serializer = SubmissionSerializer(data=data)
+    return JsonResponse(data, status=201)
+
     
-    if serializer.is_valid():
-      serializer.save()
-      return JsonResponse(serializer.data, status=201)
-    return JsonResponse(serializer.errors, status=400)
-  else:
-    return JsonResponse(serializer.errors, status=400)
+  #   if serializer.is_valid():
+  #     serializer.save()
+  #     return JsonResponse(serializer.data, status=201)
+  #   return JsonResponse(serializer.errors, status=400)
+  # else:
+  #   return JsonResponse(serializer.errors, status=400)
