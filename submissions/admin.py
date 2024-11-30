d0ee2664c2b5877920aefddc6c0ee525d0b88b08
KJ Osenenko <kj@mac-kj.local>
2024-11-30 12:29:03 -0700

    remove messages, drop submissions table

--- submissions/admin.py
+++ submissions/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-
-from .models import Submission
-
-admin.site.register(Submission)
--- submissions/models.py
+++ submissions/models.py
@@ -4,17 +4,9 @@ from django.dispatch import receiver
 import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from bands.models import Band
 
 class Submission(models.Model):
-  contact_name = models.CharField(max_length=128, blank=True, null=True)
-  contact_email = models.CharField(max_length=128, blank=True, null=True)
-  contact_phone = models.CharField(max_length=128, blank=True, null=True)
-  message_subject = models.CharField(max_length=128, blank=True, null=True)
-  message_body = models.TextField(blank=True, null=True)
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  band = models.ForeignKey(Band, on_delete=models.CASCADE)
+  submissions = models.Manager()
   
 @receiver(post_save, sender=Submission)
 def send_email(sender, instance, **kwargs):
